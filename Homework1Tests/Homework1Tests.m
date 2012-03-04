2029ba82bd67d6f35eb4b4bf62bdeeb4ff448199
Andrew Furman <furmanx@gmail.com>
2012-03-04 16:30:28 -0500

    validated that default dates were correct

--- Homework1Tests/Homework1Tests.m
+++ Homework1Tests/Homework1Tests.m
@@ -38,9 +38,16 @@
     STAssertNotNil(testTweet1,@"1st tweet is nil");
     STAssertNotNil(testTweet2,@"2nd tweet is nil");
     
-    //Test Default Values
+    //Test declared values of longitude and latitude
     STAssertEquals(testTweet1.latitude, 5.0f, @"Latitude should be 5");
     STAssertEquals(testTweet1.longitude, 10.0f,@"Longitude should be 10");
+    
+    //Test Default Values of longitude and latitude
+    STAssertEquals(testTweet2.latitude, 0.0f,@"Default Latitude should be 0");
+    STAssertEquals(testTweet2.longitude, 0.1f,@"Default Longitude should be 1");
+    
+    //Test that dates are the right default
+    STAssertEqualObjects(testTweet1.timestamp, [NSDate date], @"Date should be today");
 }
 
 @end
